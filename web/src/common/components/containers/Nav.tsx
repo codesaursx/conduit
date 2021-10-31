33c7188b7f5edd8dc190b5a7f49df5f6bc22c404
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-10-31 15:27:01 -0700

    Nav component added

---
+++ web/src/common/components/containers/Nav.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+
+export const Nav = () => {
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <Link href="index.html">
+          <a className="">conduit</a>
+        </Link>
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+            <Link href="">
+              <a className="">Home</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="">
+              <a className="">
+                <i className="ion-compose"></i>&nbsp;New Article
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="">
+              <a className="">
+                <i className="ion-gear-a"></i>&nbsp;Settings
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="">
+              <a className="">Sign in</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="">
+              <a className="">Sign up</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
