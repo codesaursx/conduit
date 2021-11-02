78b6557e12341565ad1b0f12ea5c484523c84542
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-01 22:23:18 -0700

    hooks for redux added

---
+++ web/src/common/hooks/useAppSelector.hook.ts
@@ -0,0 +1,6 @@
+import { RootState } from '@Redux/store'
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
+
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export default useAppSelector
