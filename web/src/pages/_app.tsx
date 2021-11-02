093aaed69442101297820dab61202d3b5ee2f16a
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-01 22:31:38 -0700

    wrappers integrated in _app file

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { Footer } from '@Components/containers/Footer'
 import { Nav } from '@Components/containers/Nav'
 import { AppProps } from 'next/dist/shared/lib/router/router'
+import { AuthWrapper } from '@Components/wrappers/AuthWrapper'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from '@Redux/store'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
-      <Nav />
-      <Component {...pageProps} />
-      <Footer />
-    </>
+    <ReduxProvider store={store}>
+      <AuthWrapper>
+        <Nav />
+        <Component {...pageProps} />
+        <Footer />
+      </AuthWrapper>
+    </ReduxProvider>
   )
 }
 
