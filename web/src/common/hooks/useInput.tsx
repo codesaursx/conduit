1bf1686495c4cf8a78d1e5525c60d9b6730d393d
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-04 18:10:54 -0700

    useInput hook created

---
+++ web/src/common/hooks/useInput.tsx
@@ -0,0 +1,35 @@
+import { TInputType } from '@Interfaces/utils.interface'
+import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react'
+
+const OPTIONAL_PLACEHOLDERS = {
+  ['password']: 'Password',
+  ['email']: 'Email',
+  ['text']: 'Your name'
+}
+
+/**
+ * Hook which gives you the whole possibly logic for an input
+ * @param type input type
+ * @returns value, onChange function, input type
+ */
+export const useInput = (type: TInputType): TUseInput => {
+  const [value, setValue] = useState('')
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  return {
+    value,
+    type,
+    placeholder: OPTIONAL_PLACEHOLDERS[type],
+    onChange
+  }
+}
+
+export type TUseInput = {
+  value: string
+  type: TInputType
+  placeholder: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+}
